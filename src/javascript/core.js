d8d97feeaaac611bb8a004d301b102de0d7720f8
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-25 20:52:47 +0200

    create contact us page

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -1,34 +1,35 @@
 // $(function () {
-  // 'use strict';
+// 'use strict';
 
-  //   bsCustomFileInput.init();
+//   bsCustomFileInput.init();
 
-  //   // ----------- toggle value on input
-  //   $('input.input-effect').focusout(function () {
-  //     toggle_input_effect(this);
-  //   });
+//   // ----------- toggle value on input
+//   $('input.input-effect').focusout(function () {
+//     toggle_input_effect(this);
+//   });
 
-  //   document.querySelectorAll('input.input-effect').forEach((item) => {
-  //     toggle_input_effect(item);
-  //   });
+//   document.querySelectorAll('input.input-effect').forEach((item) => {
+//     toggle_input_effect(item);
+//   });
 
-  //   function toggle_input_effect(selector) {
-  //     if (selector.value !== '') {
-  //       selector.classList.add('has-content');
-  //     } else {
-  //       selector.classList.remove('has-content');
-  //     }
-  //   }
+//   function toggle_input_effect(selector) {
+//     if (selector.value !== '') {
+//       selector.classList.add('has-content');
+//     } else {
+//       selector.classList.remove('has-content');
+//     }
+//   }
 
-  //   $('.profile-container').addClass('pre-enter');
-  //   setTimeout(function () {
-  //     $('.profile-container').addClass('on-enter');
-  //   }, 500);
-  //   setTimeout(function () {
-  //     $('.profile-container').removeClass('pre-enter on-enter');
-  //   }, 3000);
+//   $('.profile-container').addClass('pre-enter');
+//   setTimeout(function () {
+//     $('.profile-container').addClass('on-enter');
+//   }, 500);
+//   setTimeout(function () {
+//     $('.profile-container').removeClass('pre-enter on-enter');
+//   }, 3000);
 // });
 
+// eslint-disable-next-line max-statements
 (function () {
   'use strict';
 
@@ -39,7 +40,8 @@
     menu_links = document.querySelector('#js-main-links ul'),
     search_button = document.querySelector('#search-button'),
     search_popup = document.querySelector('#search-popup'),
-    search_popup_content = document.querySelector('#search-popup .content');
+    search_popup_content = document.querySelector('#search-popup .content'),
+    form_container = document.querySelectorAll('.custom-form-input-container .custom-form-input');
 
   search_button.onclick = function (event) {
     event.stopPropagation();
@@ -62,25 +64,54 @@
     event.stopPropagation();
   };
 
-  document.addEventListener('click', function (e) {
-    if (e.target != menu_links && e.target != close_button) {
-      if (menu_links.classList.contains('open')) {
-        close_button.classList.toggle('transform');
-        menu_links.classList.toggle('open');
-      }
-    }
+  document.addEventListener('click', window_click_event);
 
-    if (e.target != search_popup_content && e.target != search_button) {
-      if (search_popup.classList.contains('open')) {
-        search_popup.classList.toggle('open');
-      }
-    }
-  });
+  // custom form moved effect
+  if (form_container) {
+    form_container.forEach(input => {
+      input.addEventListener('blur', input_has_content);
+      input.addEventListener('focus', input_has_content);
+    });
+  }
 
   document.querySelector('#current_year').textContent = new Date()
     .getFullYear()
     .toString();
 
+  /**
+* use to add or remove animation if input has content
+* @param {Element} event the input target of effect
+* @returns {Void} no return
+*/
+  function input_has_content(event) {
+    if (event.target.value.length > 0) {
+      event.target.classList.add('has-content');
+    } else {
+      event.target.classList.remove('has-content');
+    }
+  }
+
+  function window_click_event(event) {
+    window_toggle_navbar_menu(event);
+    window_toggle_searchbar(event);
+  }
+
+  function window_toggle_navbar_menu(event) {
+    if (event.target !== menu_links && event.target !== close_button) {
+      if (menu_links.classList.contains('open')) {
+        close_button.classList.toggle('transform');
+        menu_links.classList.toggle('open');
+      }
+    }
+  }
+
+  function window_toggle_searchbar(event) {
+    if (event.target !== search_popup_content && event.target !== search_button) {
+      if (search_popup.classList.contains('open')) {
+        search_popup.classList.toggle('open');
+      }
+    }
+  }
 })();
 
 //  jQuery start from here 👾
